ced68f4c003c13fe4bfedae78f5b3b6b6eefac1f
gleibercoelho <98876793+gleibercoelho@users.noreply.github.com>
2022-05-23 20:04:39 -0300

    Add files via upload

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Informática",
+      fk_idcategorys: 1,
+      name_product: "Notebook Dell",
+      price: 5114,
+      image: "./images/produtos/notebookdell.jpg",
+      id_categorys: 1,
+      name_categorys: "Informática",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "Informática",
+      fk_idcategorys: 1,
+      name_product: "Pc Gamer Intel5",
+      price: 4770,
+      image: "./images/produtos/pcgamerintell.jpg",
+      id_categorys: 1,
+      name_categorys: "Informática",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Informática",
+      fk_idcategorys: 1,
+      name_product: "Pc All In One",
+      price: 4999,
+      image: "./images/produtos/Allinone.jpg",
+      id_categorys: 1,
+      name_categorys: "Informática",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Eletrodomésticos",
+      fk_idcategorys: 2,
+      name_product: "Refrig. Electrolux",
+      price: 3199,
+      image: "./images/produtos/refrigerador.jpg",
+      id_categorys: 2,
+      name_categorys: "Eletrodomésticos",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Eletrodomésticos",
+      fk_idcategorys: 2,
+      name_product: "Lavadoura LG",
+      price: 5299,
+      image: "./images/produtos/lavadora.jpg",
+      id_categorys: 2,
+      name_categorys: "Eletrodomésticos",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Eletrodomésticos",
+      fk_idcategorys: 2,
+      name_product: "Cervejeira Consul",
+      price: 2400,
+      image: "./images/produtos/cervejeira.jpg",
+      id_categorys: 2,
+      name_categorys: "Eletrodomésticos",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Console",
+      fk_idcategorys: 3,
+      name_product: "Playstation 5",
+      price: 4899,
+      image: "./images/produtos/playstation.jpg",
+      id_categorys: 3,
+      name_categorys: "Console",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Console",
+      fk_idcategorys: 3,
+      name_product: "Xbox Series S",
+      price: 2149,
+      image: "./images/produtos/xbox.jpg",
+      id_categorys: 3,
+      name_categorys: "Console",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Console",
+      fk_idcategorys: 4,
+      name_product: "Nintendo Switch",
+      price: 2506,
+      image: "./images/produtos/switch.jpg",
+      id_categorys: 4,
+      name_categorys: "Console",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Acessórios",
+      fk_idcategorys: 4,
+      name_product: "Secador d Cabelos",
+      price: 169,
+      image: "./images/produtos/secador.jpg",
+      id_categorys: 4,
+      name_categorys: "Acessórios",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Acessórios",
+      fk_idcategorys: 5,
+      name_product: "Barbeador Philips",
+      price: 449,
+      image: "./images/produtos/barbeador.jpg",
+      id_categorys: 5,
+      name_categorys: "Acessórios",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Acessórios",
+      fk_idcategorys: 5,
+      name_product: "Cortador d Cabelos",
+      price: 129,
+      image: "./images/produtos/maquinacabelos.jpg",
+      id_categorys: 5,
+      name_categorys: "Acessórios",
+      selected: false
+    },
+];
+
+export default productsList;
